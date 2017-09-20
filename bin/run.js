f05502157382b8ae0fa8cefa36b0aa9e87e6c12d
Nikhil Garg <nikhilgarg72@gmail.com>
2017-09-20 18:00:09 +0530

    Added announce method to announce to iris server about the microservice iris-time

--- bin/run.js
+++ bin/run.js
@@ -1,12 +1,28 @@
 'use strict';
 
+const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
 
-server.listen(3002);
+server.listen();
 
 server.on('listening', function(){
-    console.log(`IRIS-Time is listenong on ${server.address().port} in ${service.get('env')} mode`); 
+    console.log(`IRIS-Time is listenong on ${server.address().port} in ${service.get('env')} mode`);
+    
+    const announce = () => {
+        request.put(`http://127.0.0.1:3001/service/time/${server.address().port}`, (err, res)=>{
+            if(err){
+                console.log(err);
+                console.log('There was an error connecting to Iris');
+                return;
+            }
+            console.log(res.body);
+        });
+    }
+
+    announce();
+
+    setInterval(announce, 15*1000);
 });
