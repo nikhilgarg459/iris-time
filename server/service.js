8903db823794ff396429385bc01b386965d9f069
Nikhil Garg <nikhilgarg72@gmail.com>
2017-09-20 16:30:05 +0530

    Google map API integrated to get lat and long for a given location

--- server/service.js
+++ server/service.js
@@ -2,9 +2,19 @@
 
 const express = require('express');
 const service = express();
+const request = require('superagent');
+
+const apiKey = require('../const.js').mapKey;
 
 service.get('/service:location', (req, res, next) =>{
-    res.json({result: req.params.location});
+    request.get('https://maps.googleapis.com/maps/api/geocode/json?address='+ req.params.location +'&key=' + apiKey, (err, response) =>{
+        if(err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.json(response.body.results[0].geometry.location);
+        
+    });
 });
 
 module.exports = service;
