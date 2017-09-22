2fb0026f1fdb55d9f792636b136096d4ed0e45b4
Nikhil Garg <nikhilgarg72@gmail.com>
2017-09-22 19:51:13 +0530

    Added endpoint security

--- bin/run.js
+++ bin/run.js
@@ -10,21 +10,24 @@ const server = http.createServer(service);
 
 server.listen();
 
-server.on('listening', function(){
+server.on('listening', function () {
     log.info(`IRIS-Time is listenong on ${server.address().port} in ${service.get('env')} mode`);
-    
+
     const announce = () => {
-        request.put(`http://127.0.0.1:3001/service/time/${server.address().port}`, (err, res)=>{
-            if(err){
-                log.debug(err);
-                log.info('There was an error connecting to Iris');
-                return;
-            }
-            log.info(res.body);
-        });
+        request.put(`http://127.0.0.1:3001/service/time/${server.address().port}`)
+            .set('X-IRIS-SERVICE-TOKEN', config.serviceAccessToken)
+            .set('X-IRIS-API-TOKEN', config.irisApiToken)
+            .end((err, res) => {
+                if (err) {
+                    log.debug(err);
+                    log.info('There was an error connecting to Iris');
+                    return;
+                }
+                log.info(res.body);
+            });
     };
 
     announce();
 
-    setInterval(announce, 15*1000);
+    setInterval(announce, 15 * 1000);
 });
--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const bunyan = require('bunyan');
+const serviceAccessToken = require('crypto').randomBytes(16).toString('hex').slice(0, 32);
 const log = {
     development: ()=>{
         return bunyan.createLogger({name: 'IRIS-TIME-development', level: 'debug'});
@@ -17,6 +18,8 @@ const log = {
 module.exports = {
     googleGeoApiKey : process.env.GOOGLE_GEO_API_KEY,
     googleTimeApiKey : process.env.GOOGLE_TIME_API_KEY,
+    irisApiToken: process.env.IRIS_API_TOKEN,
+    serviceAccessToken: serviceAccessToken,
     log: (env)=>{
         if(env) return log[env]();
         return log[process.env.NODE_ENV || 'development']();
