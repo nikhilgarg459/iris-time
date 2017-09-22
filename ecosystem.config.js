04ce46fd4978c03a94d1b784c842e1d997bf4dd5
Nikhil Garg <nikhilgarg72@gmail.com>
2017-09-23 01:13:42 +0530

    updated pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,14 +5,14 @@ module.exports = {
             script: 'bin/run.js',
             env_production: {
                 NODE_ENV: 'production',
-                IRIS_URL: 'http://52.15.127.7:3001'
+                IRIS_URL: 'http://172-31-1-42:3001'
             }
         }
     ],
     deploy: {
         production: {
             user: 'node',
-            host: '18.221.45.30',
+            host: '172-31-3-208',
             ref: 'origin/master',
             repo: 'https://github.com/nikhilgarg459/iris-time.git',
             path: '/srv/production',
